1e92315f2437bb10daffd9d70a23a544efd2e003
Dmitrii Altukhov <dm.altukhov@ya.ru>
2021-10-25 18:26:59 +0300

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Aleksandra Kuznetsova on 25.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
